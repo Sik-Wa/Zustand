f8929e98b2ccd610a801622f1123ea48cd74b1fa
Sik-Wa <sikwavivian@gmail.com>
2025-05-03 12:24:30 +0200

    ui works

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,13 +9,13 @@ const Button = () => {
         <div className=" mt-10 flex items-center justify-evenly ">
           <button
             onClick={increment}
-            className=" rounded-md bg-black/60 text-white px-2 py-1 "
+            className=" rounded-md bg-black/60 text-white hover:bg-green-500 hover:text-black px-2 py-1 "
           >
             inc +{" "}
           </button>
           <button
             onClick={decrement}
-            className=" rounded-md bg-black/60 text-white px-2 py-1"
+            className=" hover:bg-red-500 hover:text-black rounded-md bg-black/60 text-white px-2 py-1"
           >
             dec -
           </button>
